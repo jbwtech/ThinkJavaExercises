e57aba1e09c3540be40314f4974e72978a9f97a3
Brent Williams <jbwtech@gmail.com>
2019-04-01 13:50:11 -0400

    updates

--- Time.java
+++ Time.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 
 /**
  * Write a description of class Time here.
@@ -7,12 +8,28 @@
  */
 public class Time
 {
+    final int SECONDS_PER_MINUTE = 60;
+    final int SECONDS_PER_HOUR = 3600;
+    final int SECONDS_PER_DAY = 86400;
+
+    int hour, minute, second;
     /**
      * Constructor for objects of class Time
      */
-    public Time()
-    {
+    public Time() {
+        hour = 13;
+        minute = 29;
+        second = 0;
     }
 
-    
+    public void main() {
+        int total = 0;
+
+        total = (hour * SECONDS_PER_HOUR) + (minute * SECONDS_PER_MINUTE) + second;
+        System.out.printf("The time is %d:%d\n", hour,minute);
+        System.out.printf("It has been %d seconds since midnight.\n", total);
+        System.out.printf("There are %d seconds remaining in the day.\n", (SECONDS_PER_DAY - total));
+        System.out.printf("%f percent of the day has passed.\n", ((double)(total)/SECONDS_PER_DAY)*100);
+    }
+
 }
